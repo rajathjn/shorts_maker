1dce19b63db4c9a7e30b00fc42e4596d61a8d41b
rajathjn <rajathjnx@gmail.com>
2025-02-26 23:12:19 +0530

    "Add new utilities and update __all__ exports"

--- ShortsMaker/__init__.py
+++ ShortsMaker/__init__.py
@@ -1,26 +1,39 @@
 from .ask_llm import AskLLM, OllamaServiceManager
 from .generate_image import GenerateImage
-from .shorts_maker import ShortsMaker, abbreviation_replacer, has_alpha_and_digit, split_alpha_and_digit
+from .moviepy_create_video import MoviepyCreateVideo
+from .shorts_maker import (
+    ShortsMaker,
+    abbreviation_replacer,
+    has_alpha_and_digit,
+    split_alpha_and_digit,
+)
 from .utils.audio_transcript import (
     align_transcript_with_script,
     generate_audio_transcription,
 )
+from .utils.colors_dict import colors_dict
+from .utils.download_youtube_music import download_youtube_music
+from .utils.download_youtube_video import download_youtube_video
 from .utils.notify_discord import notify_discord
 from .utils.retry import retry
 from .utils.tiktokvoice import VOICES, tts
 
 __all__ = [
-    "GenerateImage",
-    "ShortsMaker",
-    "AskLLM",
-    "OllamaServiceManager",
-    "abbreviation_replacer",
-    "has_alpha_and_digit",
-    "split_alpha_and_digit",
-    "align_transcript_with_script",
-    "generate_audio_transcription",
-    "notify_discord",
-    "retry",
-    "VOICES",
-    "tts",
+    GenerateImage,
+    ShortsMaker,
+    AskLLM,
+    OllamaServiceManager,
+    MoviepyCreateVideo,
+    download_youtube_music,
+    download_youtube_video,
+    abbreviation_replacer,
+    colors_dict,
+    has_alpha_and_digit,
+    split_alpha_and_digit,
+    align_transcript_with_script,
+    generate_audio_transcription,
+    notify_discord,
+    retry,
+    VOICES,
+    tts,
 ]
--- ShortsMaker/utils/__init__.py
+++ ShortsMaker/utils/__init__.py
@@ -1,13 +1,21 @@
 from .audio_transcript import align_transcript_with_script, generate_audio_transcription
+from .colors_dict import colors_dict
+from .download_youtube_music import download_youtube_music
+from .download_youtube_video import download_youtube_video
+from .logging_config import setup_package_logging
 from .notify_discord import notify_discord
 from .retry import retry
 from .tiktokvoice import VOICES, tts
 
 __all__ = [
     align_transcript_with_script,
+    download_youtube_music,
+    download_youtube_video,
     generate_audio_transcription,
+    setup_package_logging,
     notify_discord,
     retry,
-    VOICES,
     tts,
+    colors_dict,
+    VOICES,
 ]
