d6d141336a6f8cdb5c3102b6874c9c934b75333e
rajathjn <rajathjnx@gmail.com>
2025-02-26 23:19:04 +0530

    "Add Flux Pixel Wave support and optimize model unloading"

--- ShortsMaker/generate_image.py
+++ ShortsMaker/generate_image.py
@@ -1,18 +1,22 @@
 # https://huggingface.co/docs/diffusers/main/en/index
 import logging
 import os
+from collections import defaultdict
 from pathlib import Path
-from typing import DefaultDict, Optional, Union
+from time import sleep
 
 import torch
 import yaml
-from diffusers import FluxPipeline
+from diffusers import AutoencoderKL, FluxPipeline
+from transformers import CLIPTextModel, T5EncoderModel
 
-from .logging_config import setup_package_logging
+from .utils import setup_package_logging
+
+MODEL_UNLOAD_DELAY = 5
 
 
 class GenerateImage:
-    def __init__(self, config_file: Union[Path, str], logging_config: DefaultDict = None) -> None:
+    def __init__(self, config_file: Path | str, logging_config: defaultdict = None) -> None:
         # if config_file is str convert it to a Pathlike
         self.setup_cfg = Path(config_file) if isinstance(config_file, str) else config_file
 
@@ -23,7 +27,7 @@ class GenerateImage:
             raise ValueError(f"File {str(self.setup_cfg)} is not a yaml file")
 
         # load the yml file
-        with open(self.setup_cfg, "r") as f:
+        with open(self.setup_cfg) as f:
             self.cfg = yaml.safe_load(f)
 
         # check if logging is set up in config_file
@@ -47,9 +51,11 @@ class GenerateImage:
         self.logger = setup_package_logging(**self.logging_cfg)
 
         if "hugging_face_access_token" not in self.cfg:
-            self.logger.warning("Please add your huggingface access token to use Flux.1-Dev.\nDefaulting to use Flux.1-Schnell")
+            self.logger.warning(
+                "Please add your huggingface access token to use Flux.1-Dev.\nDefaulting to use Flux.1-Schnell"
+            )
 
-        self.pipe: Optional[FluxPipeline] = None
+        self.pipe: FluxPipeline | None = None
 
     def _load_model(self, model_id: str) -> bool:
         try:
@@ -111,6 +117,8 @@ class GenerateImage:
         self.pipe = None
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
+        self.logger.info("Wait for 5 seconds, So that the GPU memory can be freed")
+        sleep(MODEL_UNLOAD_DELAY)
         return True
 
     # @retry(max_retries=MAX_RETRIES, delay=DELAY, notify=NOTIFY)
@@ -120,7 +128,7 @@ class GenerateImage:
         output_path: str,
         negative_prompt: str = "",
         model_id: str = "black-forest-labs/FLUX.1-schnell",
-        steps: int = 20,
+        steps: int = 4,
         seed: int = 0,
         height: int = 1024,
         width: int = 1024,
@@ -149,4 +157,88 @@ class GenerateImage:
         self.pipe = None
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
+        self.logger.info("Wait for 5 seconds, So that the GPU memory can be freed")
+        sleep(MODEL_UNLOAD_DELAY)
         return True
+
+    def use_flux_pixel_wave(
+        self,
+        prompt: str,
+        output_path: str,
+        model_id: str = "https://huggingface.co/mikeyandfriends/PixelWave_FLUX.1-schnell_03/blob/main/pixelwave_flux1_schnell_fp8_03.safetensors",
+        steps: int = 4,
+        seed: int = 595570113709576,
+        height: int = 1024,
+        width: int = 1024,
+        guidance_scale: float = 3.5,
+    ) -> bool:
+        self.logger.info("This image generator uses the Flux Pixel Wave model.")
+
+        text_encoder = CLIPTextModel.from_pretrained(
+            "black-forest-labs/FLUX.1-schnell", subfolder="text_encoder"
+        )
+        text_encoder_2 = T5EncoderModel.from_pretrained(
+            "black-forest-labs/FLUX.1-schnell", subfolder="text_encoder_2"
+        )
+        vae = AutoencoderKL.from_pretrained("black-forest-labs/FLUX.1-schnell", subfolder="vae")
+
+        self.pipe = FluxPipeline.from_single_file(
+            model_id,
+            use_safetensors=True,
+            torch_dtype=torch.bfloat16,
+            # Load additional not included in safetensor
+            text_encoder=text_encoder,
+            text_encoder_2=text_encoder_2,
+            vae=vae,
+        )
+
+        # to run on low vram GPUs (i.e. between 4 and 32 GB VRAM)
+        # Choose ONE of the following:
+        # pipe.enable_model_cpu_offload()  # Best for low-VRAM GPUs
+        self.pipe.enable_sequential_cpu_offload()  # Alternative for moderate VRAM GPUs
+        self.pipe.vae.enable_slicing()  # Reduces memory usage for decoding
+        self.pipe.vae.enable_tiling()  # Further optimizes VAE computation
+
+        # casting here instead of in the pipeline constructor because doing so in the constructor loads all models into CPU memory at once
+        self.pipe.to(torch.float16)
+
+        self.logger.info("Generating image")
+        image = self.pipe(
+            prompt,
+            guidance_scale=guidance_scale,
+            output_type="pil",
+            num_inference_steps=steps,
+            max_sequence_length=256,
+            height=height,
+            width=width,
+            generator=torch.Generator("cpu").manual_seed(seed),
+        ).images[0]
+        image.save(output_path)
+        self.logger.info(f"Image saved to {output_path}")
+
+        del self.pipe
+        del text_encoder
+        del text_encoder_2
+        del vae
+
+        self.pipe = None
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+        self.logger.info("Wait for 5 seconds, So that the GPU memory can be freed")
+        sleep(MODEL_UNLOAD_DELAY)
+        return True
+
+    def quit(self) -> None:
+        self.logger.info("Quitting the image generator")
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+        # Delete all instance variables
+        for attr in list(self.__dict__.keys()):
+            try:
+                self.logger.debug(f"Deleting {attr}")
+                if attr == "logger":
+                    continue
+                delattr(self, attr)
+            except Exception as e:
+                self.logger.error(f"Error deleting {attr}: {e}")
+        return None
